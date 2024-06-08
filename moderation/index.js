a66886521c983baf148e158946dd073b2c30b3f0
mwibutsa <mflohost@gmail.com>
2024-06-08 13:11:27 +0200

    After adding skaffold

--- moderation/index.js
+++ moderation/index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post('/events', async (req, res) => {
+app.post("/events", async (req, res) => {
   const { type, data } = req.body;
-  if (type === 'CommentCreated') {
-    const status = data.content.includes('orange') ? 'rejected' : 'approved';
-    await axios.post('http://localhost:4005/events', {
-      type: 'CommentModerated',
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+    await axios.post("http://event-bus-srv:4005/events", {
+      type: "CommentModerated",
       data: { ...data, status },
     });
   }
-  console.log('Moderated');
+  console.log("Moderated");
   res.send({});
 });
 
 app.listen(4003, () => {
-  console.log('listening on http://localhost:4003');
+  console.log("listening on http://localhost:4003");
 });
--- posts/index.js
+++ posts/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -10,28 +10,29 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
-  const id = randomBytes(4).toString('hex');
+app.post("/posts/create", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = { id, title };
 
-  await axios.post('http://localhost:4005/events', {
-    type: 'PostCreated',
+  await axios.post("http://event-bus-srv:4005/events", {
+    type: "PostCreated",
     data: { id, title },
   });
 
-  res.status(201).send(posts[id]);
+  return res.status(201).send(posts[id]);
 });
 
-app.post('/events', async (req, res) => {
-  console.log('Received Event', req.body);
-  res.send({});
+app.post("/events", async (req, res) => {
+  console.log("Received Event", req.body);
+  return res.send({});
 });
 
 app.listen(4000, () => {
-  console.log('listening on 4000');
+  console.log("v50");
+  console.log("listening on 4000");
 });
